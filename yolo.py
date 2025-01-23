cb514c440d3570a0e6c8fcd7981b4b259c6050fd
Minh Huy Nguyen <minhhuy210108@gmail.com>
2025-01-23 09:34:15 +0700

    update

---
+++ yolo.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+
+HUGGINGFACE = os.getenv("HUGGINGFACE")
+API_URL = "https://api-inference.huggingface.co/models/dima806/facial_emotions_image_detection"
+headers = {"Authorization": f"Bearer {HUGGINGFACE}"}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+output = query("cats.jpg")
