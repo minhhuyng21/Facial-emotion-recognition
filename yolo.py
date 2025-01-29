d874922893024d7ff5b84141954a62078ce55edb
Minh Huy Nguyen <minhhuy210108@gmail.com>
2025-01-29 21:21:56 +0700

    ,

--- yolo.py
+++ yolo.py
@@ -1,15 +1,8 @@
-import requests
-import os
+from deepface import DeepFace
 
+objs = DeepFace.analyze(
+  img_path = "DSC_6883.jpg", 
+  actions = ['emotion'],
+)
 
-HUGGINGFACE = os.getenv("HUGGINGFACE")
-API_URL = "https://api-inference.huggingface.co/models/dima806/facial_emotions_image_detection"
-headers = {"Authorization": f"Bearer {HUGGINGFACE}"}
-
-def query(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
-
-output = query("cats.jpg")
+print(objs)
